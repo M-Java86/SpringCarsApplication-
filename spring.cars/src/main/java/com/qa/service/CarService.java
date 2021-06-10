a67f1ebdc164054314089b299be742bd5152c4b2
M-java86 <mpythondata@outlook.com>
2021-06-10 15:20:59 -0400

    MO: added CRUD FUNCTIONALITY

--- spring.cars/src/main/java/com/qa/service/CarService.java
+++ spring.cars/src/main/java/com/qa/service/CarService.java
@@ -1,5 +1,52 @@
 package com.qa.service;
 
 public class CarService {
+	
+	
+	@Service
+	public class CarService {
+			private CarRepo repo;
+			
+			public CarService(CarRepo repo) {
+				this.repo = repo;
+			}
+			
+		// CRUD
+		// CREATE
+			
+		public Car createVehicle(Car c ) {
+			return this.repo.save(c);
+			//insert into Car values (c.getMake(), c.getModel(), c.getYear(), c.getDoors());
+		}
+		
+		// Read
+		public List<Car> getAllVehicles() {
+			return this.repo.findAll();
+		}
+		
+		// READ ONE
+		public Car getVehicle(Long id) {
+			optional<Car> c = this.repo.findBYID(id);
+			return c.get();
+			
+			
+	////  optional<Car> c = this.repo.findBYID(id);
+			//if(c != null) {
+					//return c;
+			//} else {
+				//return null;
+			}
+		
+		}
+		
+		// DELETE 
+		public boolean renoveVehicle(long id) {
+			this.repo.deleteByID(id);
+			return this.repo.exist
+		
+		
+		}
+	}
+	
 
 }
