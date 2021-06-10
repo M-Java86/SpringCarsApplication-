bb00b91501353d50faff23d52c29a60681bb3950
M-java86 <mpythondata@outlook.com>
2021-06-10 00:02:55 -0400

    MO: Added and generated getters and setters

--- spring.cars/src/main/java/com/qa/springcars/domain/Car.java
+++ spring.cars/src/main/java/com/qa/springcars/domain/Car.java
@@ -2,12 +2,11 @@ package com.qa.springcars.domain;
 
 public class Car {
 	
+	//private- they can only be seen in the car class , interact with these attributes outside the car class
+	//getters and setters
 	private String Make;
-	
 	private String model;
-	
 	private int year;
-	
 	private int doors;
 
 }
