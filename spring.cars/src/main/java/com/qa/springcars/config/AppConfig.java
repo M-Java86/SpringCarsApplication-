ab6af454edbbc7d264cb3a3f64aee5aadc47514c
M-java86 <mpythondata@outlook.com>
2021-06-09 23:37:44 -0400

    MO: added @Bean

--- spring.cars/src/main/java/com/qa/springcars/config/AppConfig.java
+++ spring.cars/src/main/java/com/qa/springcars/config/AppConfig.java
@@ -1,12 +1,15 @@
 package com.qa.springcars.config;
 
 import java.time.LocalTime;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class AppConfig {
 	
+	@Bean
 	public String sayHello() {
 		return "Michelle says hi";
 }
