b79ce36bcba29cf525bf60deeb501d82464bcc35
M-java86 <mpythondata@outlook.com>
2021-06-10 00:13:53 -0400

    MO: Added and Generated Constructors

--- spring.cars/src/main/java/com/qa/springcars/domain/Car.java
+++ spring.cars/src/main/java/com/qa/springcars/domain/Car.java
@@ -8,5 +8,50 @@ public class Car {
 	private String model;
 	private int year;
 	private int doors;
+	
+	//Default Constructor 
+	public Car() {
+		
+	}
+	
+	
+	
+	// Constructor 
+	public Car(String make, String model, int year, int doors) {
+		super();
+		Make = make;
+		this.model = model;
+		this.year = year;
+		this.doors = doors;
+		
+	}
+	
+	// getters and setters - interact with attributes 
+	public String getMake() {
+		return Make;
+	}
+	public void setMake(String make) {
+		Make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getDoors() {
+		return doors;
+	}
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+	
+	
 
 }
