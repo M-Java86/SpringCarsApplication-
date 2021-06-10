0814541891026f0fd07fa2d2338856714aaa3990
M-java86 <mpythondata@outlook.com>
2021-06-10 14:43:18 -0400

    MO: added @Services

--- spring.cars/src/main/java/com/qa/service/CarServiceList.java
+++ spring.cars/src/main/java/com/qa/service/CarServiceList.java
@@ -2,8 +2,11 @@ package com.qa.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.qa.springcars.domain.Car;
 
+@Service // main business logic of our application lives!
 public class CarServiceList {
 
 	// Here is where our logic will go 
@@ -11,8 +14,40 @@ public class CarServiceList {
 	
 	private List<Car> vehicles; // <--- dependency 
 	
-	public CarServiceList(List<Car>) vehicles) {
-		this.vehicle = vehicles;
+	//Constructor 
+	public CarServiceList(List<Car> vehicles) {
+		this.vehicles = vehicles;
 	}
+	
+	// CREATE
+	public Car createVehicle(Car car) {
+		this.vehicles.add(car);
+		// Grab the last thing that was added to the list 
+		Car added = this.vehicles.get(this.vehicles.size() -1);
+		return added;
+	}
+	
+	//Read
+	public List<Car> getAllVehicles() {
+		return this.vehicles;
+	}
+	
+	//Read one 
+	public Car getVehicle(int index) {
+		return this.vehicles.get(index);
+	}
+	
+	//DELETE
+	public boolean removeVehicle(int index) {
+			this.vehicles.remove(index);
+			return true;
+	}
+	
+	// PUT - replace the whole thing
+	public Car updateVehicle(int index, Car newCar) {
+		this.vehicles.set(index,  newCar);
+		return newCar;
+	}
+	
 	
 }
