421aa5b623ced08a2e03fbddf8d7245cee0552b3
M-java86 <mpythondata@outlook.com>
2021-06-09 22:54:46 -0400

    MO

--- spring.cars/src/main/java/com/qa/springcars/SpringCarsApplication.java
+++ spring.cars/src/main/java/com/qa/springcars/SpringCarsApplication.java
@@ -1,5 +1,7 @@
 package com.qa.springcars;
 
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +14,8 @@ public class SpringCarsApplication {
 		ApplicationContext beanbag = SpringApplication.run(SpringCarsApplication.class, args);
 		
 		System.out.println(beanbag.containsBean("greeting"));
+		System.out.println(beanbag.containsBean("getCurrent"));
+		
 	}
 
 	
@@ -22,6 +26,11 @@ public class SpringCarsApplication {
 	@Bean
 	public String greeting() {
 		return "Hello World";
+	}
+	
+	@Bean
+	public  LocalTime getCurrent() {
+			return LocalTime.now();
 	}
 	
 }
