663e1169065728e643e811e9078a0c1c553c75a4
M-java86 <mpythondata@outlook.com>
2021-06-09 23:02:12 -0400

    MO: initial commit

--- spring.cars/src/main/java/com/qa/springcars/controller/CarController.java
+++ spring.cars/src/main/java/com/qa/springcars/controller/CarController.java
@@ -1,5 +1,11 @@
 package com.qa.springcars.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 public class CarController {
 
+	@RestController
+	public String sayHello() {
+		return "Michelle says hi";
+	}
 }
