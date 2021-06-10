99cdf1c91809f4d77ab9a0ab5514e960e43b724d
M-java86 <mpythondata@outlook.com>
2021-06-09 22:46:36 -0400

    MO: added @Bean, "Hello World";

--- spring.cars/src/main/java/com/qa/springcars/SpringCarsApplication.java
+++ spring.cars/src/main/java/com/qa/springcars/SpringCarsApplication.java
@@ -2,12 +2,26 @@ package com.qa.springcars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCarsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCarsApplication.class, args);
+		ApplicationContext beanbag = SpringApplication.run(SpringCarsApplication.class, args);
+		
+		System.out.println(beanbag.containsBean("greeting"));
 	}
 
+	
+	
+	//Bean- managed object 
+	//meaning the creation, maintenance, and destruction of the object is ALL done by Spring 
+	
+	@Bean
+	public String greeting() {
+		return "Hello World";
+	}
+	
 }
