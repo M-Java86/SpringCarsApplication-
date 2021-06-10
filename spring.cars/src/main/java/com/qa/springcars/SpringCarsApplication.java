7e6f6000027615e13403f283174fbd6d68815062
M-java86 <mpythondata@outlook.com>
2021-06-09 23:31:50 -0400

    MO: comment out

--- spring.cars/src/main/java/com/qa/springcars/SpringCarsApplication.java
+++ spring.cars/src/main/java/com/qa/springcars/SpringCarsApplication.java
@@ -4,23 +4,23 @@ import java.time.LocalTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+//import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import com.qa.springcars.controller.CarController;
+//import com.qa.springcars.controller.CarController;
 
 @SpringBootApplication
 public class SpringCarsApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext beanbag = SpringApplication.run(SpringCarsApplication.class, args);
+		SpringApplication.run(SpringCarsApplication.class, args);
 		
 		// VERY SKETCHY - PLEASE DON'T USE IN YOUR APP
-		System.out.println(beanbag.containsBean("greeting"));
-		System.out.println(beanbag.containsBean("getCurrent"));
+		//System.out.println(beanbag.containsBean("greeting"));
+		//System.out.println(beanbag.containsBean("getCurrent"));
 		
-		CarController controller = beanbag.getBean(CarController.class);
-		System.out.println(controller.sayHello());
+		//CarController controller = beanbag.getBean(CarController.class);
+		//System.out.println(controller.sayHello());
 		
 	}
 
