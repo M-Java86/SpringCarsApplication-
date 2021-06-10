a271c8de1b86700a7c9bcb096d764b47d4dda089
M-java86 <mpythondata@outlook.com>
2021-06-09 23:39:06 -0400

    MO: imported @Scope

--- spring.cars/src/main/java/com/qa/springcars/config/AppConfig.java
+++ spring.cars/src/main/java/com/qa/springcars/config/AppConfig.java
@@ -4,12 +4,14 @@ import java.time.LocalTime;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 
 @Configuration
 public class AppConfig {
 	
 	@Bean
+	@Scope("prototype") // create multiple instances of it 
 	public String sayHello() {
 		return "Michelle says hi";
 }
