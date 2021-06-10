0c45004009fa1bf4a53b6c4d4919e774f2753242
M-java86 <mpythondata@outlook.com>
2021-06-10 00:25:32 -0400

    MO: added and imported java util list, ArrayList and @PostMapping to createVehicle

--- spring.cars/src/main/java/com/qa/springcars/controller/CarController.java
+++ spring.cars/src/main/java/com/qa/springcars/controller/CarController.java
@@ -1,7 +1,13 @@
 package com.qa.springcars.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.springcars.domain.Car;
 
 @RestController //Allow eternal access to our application via HTTPSRequests
 public class CarController {
@@ -13,9 +19,21 @@ public class CarController {
 			
 		}
         
+        private List<Car> vehicles = new ArrayList<>();
+        
+        //CRUD Operations
         //List - Create a new Car - Add a Car to a List 
         // Read a car from a list 
         // Update a car 
         // Delete a car 
+        
+        // CREATE
+        
+        @PostMapping("/create")
+        public String createVehicle(Car car) {
+        	this.vehicles.add(car);
+        	return "Successfully Added Car";
+        	
+        }
 	}
 
