aa06c6bc9699256abc7023aca04dfc75cea80d2a
M-java86 <mpythondata@outlook.com>
2021-06-09 23:31:23 -0400

    MO: refactor

--- spring.cars/src/main/java/com/qa/springcars/controller/CarController.java
+++ spring.cars/src/main/java/com/qa/springcars/controller/CarController.java
@@ -1,11 +1,19 @@
 package com.qa.springcars.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 public class CarController {
 
-	@RestController
+	//@RestControllerAdvice
 	public String sayHello() {
-		return "Michelle says hi";
+		//return "Michelle says hi";
+		
+		//@GetMapping("/hello") //@TypeofRequest("location)
+		//public String sayHi() {
+			return "Hello, World";
+			
+		}
 	}
-}
+//}
