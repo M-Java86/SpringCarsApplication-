ddee88fa5fcd5c863221d9f8db7f4d9545bfaa05
M-java86 <mpythondata@outlook.com>
2021-06-09 23:36:17 -0400

    MO: added @Configuration

--- spring.cars/src/main/java/com/qa/springcars/config/AppConfig.java
+++ spring.cars/src/main/java/com/qa/springcars/config/AppConfig.java
@@ -1,5 +1,19 @@
 package com.qa.springcars.config;
 
+import java.time.LocalTime;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class AppConfig {
+	
+	public String sayHello() {
+		return "Michelle says hi";
+}
+	
+	public  LocalTime getCurrent() {
+		return LocalTime.now();
+
+}
 
 }
