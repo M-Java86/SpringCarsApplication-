5ed2dd95235b7e25cfc49e6f2dea654cb4de04ab
M-java86 <mpythondata@outlook.com>
2021-06-10 10:01:14 -0400

    MO: initial commit

---
+++ spring.cars/src/main/java/com/qa/service/CarServiceList.java
@@ -0,0 +1,16 @@
+package com.qa.service;
+
+import java.util.List;
+
+import com.qa.springcars.domain.Car;
+
+public class CarServiceList {
+
+	// Here is where our logic will go 
+	
+	
+	private List<Car> vehicles; // <--- dependency 
+	
+	public CarService(List)
+	
+}
